9c21a6309e56011e81010fae15b1062f01d2a32f
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2019-07-23 21:02:40 +0200

    commit

--- src/me/mrletsplay/webinterfaceapi/webinterface/Webinterface.java
+++ src/me/mrletsplay/webinterfaceapi/webinterface/Webinterface.java
@@ -1,6 +1,8 @@
 package me.mrletsplay.webinterfaceapi.webinterface;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import me.mrletsplay.webinterfaceapi.http.HttpServer;
 import me.mrletsplay.webinterfaceapi.http.document.FileDocument;
@@ -13,12 +15,16 @@ import me.mrletsplay.webinterfaceapi.webinterface.page.element.WebinterfaceEleme
 public class Webinterface {
 	
 	private static HttpServer server = new HttpServer(8080);
+	private static List<WebinterfacePage> pages;
 	
 	static {
 		server.getDocumentProvider().registerDocument("/_internal/include.js", new FileDocument(new File("TEST/script.js"), "application/javascript"));
 		server.getDocumentProvider().registerDocument("/_internal/include.css", new FileDocument(new File("TEST/style.css"), "text/css"));
+		server.getDocumentProvider().registerDocument("/_internal/header.png", new FileDocument(new File("TEST/webinterfaceapi.png"), "image/png"));
 		
-		WebinterfacePage p = new WebinterfacePage("Test page");
+		pages = new ArrayList<>();
+		
+		WebinterfacePage p = new WebinterfacePage("Test page", "/test");
 		WebinterfacePageSection s = new WebinterfacePageSection();
 		
 		WebinterfaceElementGroup g = new WebinterfaceElementGroup();
@@ -29,7 +35,7 @@ public class Webinterface {
 		g.addElement(b);
 		
 		WebinterfaceButton b2 = new WebinterfaceButton("Save");
-		b2.setWidth("300px");
+		b2.setWidth("200px");
 		b2.setLayout(WebinterfaceElementLayout.FULL_WIDTH);
 		g.addElement(b2);
 
@@ -40,7 +46,14 @@ public class Webinterface {
 		
 		p.addSection(s);
 		p.addSection(s);
-		server.getDocumentProvider().registerDocument("/", p);
+		p.addSection(s);
+		p.addSection(s);
+		
+		registerPage(p);
+		
+		WebinterfacePage p2 = new WebinterfacePage("Test2 page", "/test2");
+		p2.addSection(s);
+		registerPage(p2);
 	}
 	
 	public static void start() {
@@ -49,6 +62,15 @@ public class Webinterface {
 	
 	public static HttpServer getServer() {
 		return server;
+	}
+	
+	public static void registerPage(WebinterfacePage page) {
+		pages.add(page);
+		server.getDocumentProvider().registerDocument(page.getUrl(), page);
+	}
+	
+	public static List<WebinterfacePage> getPages() {
+		return pages;
 	}
 	
 	public static void shutdown() {
--- src/me/mrletsplay/webinterfaceapi/webinterface/page/WebinterfacePage.java
+++ src/me/mrletsplay/webinterfaceapi/webinterface/page/WebinterfacePage.java
@@ -8,6 +8,7 @@ import me.mrletsplay.webinterfaceapi.html.HtmlElement;
 import me.mrletsplay.webinterfaceapi.http.document.HttpDocument;
 import me.mrletsplay.webinterfaceapi.http.request.HttpRequestContext;
 import me.mrletsplay.webinterfaceapi.js.JavaScriptScript;
+import me.mrletsplay.webinterfaceapi.webinterface.Webinterface;
 
 public class WebinterfacePage implements HttpDocument {
 	
@@ -15,33 +16,83 @@ public class WebinterfacePage implements HttpDocument {
 		PROPERTY_DOCUMENT = "webinterface-document",
 		PROPERTY_SCRIPT = "webinterface-script";
 	
-	private String name;
+	private String name, url;
 	private List<WebinterfacePageSection> sections;
 	
-	public WebinterfacePage(String name) {
+	public WebinterfacePage(String name, String url) {
 		this.name = name;
+		this.url = url;
 		this.sections = new ArrayList<>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getUrl() {
+		return url;
 	}
 	
 	public void addSection(WebinterfacePageSection section) {
 		sections.add(section);
 	}
 	
+	public List<WebinterfacePageSection> getSections() {
+		return sections;
+	}
+	
 	public HtmlDocument toHtml() {
 		HtmlDocument d = new HtmlDocument();
 		d.setTitle(name);
+		d.setLanguage("en");
 		JavaScriptScript sc = new JavaScriptScript();
 		
 		HttpRequestContext ctx = HttpRequestContext.getCurrentContext();
 		ctx.setProperty(PROPERTY_DOCUMENT, d);
 		ctx.setProperty(PROPERTY_SCRIPT, sc);
 		
+		HtmlElement header = new HtmlElement("header");
+		header.addClass("header");
+		
+		HtmlElement img = new HtmlElement("img");
+		img.setAttribute("src", "/_internal/header.png");
+		header.appendChild(img);
+		
+		HtmlElement main = new HtmlElement("main");
+		main.addClass("main");
+		
+		HtmlElement content = new HtmlElement("div");
 		for(WebinterfacePageSection s : sections) {
-			d.getBodyNode().appendChild(s.toHtml());
+			content.appendChild(s.toHtml());
+		}
+		content.addClass("content-container");
+		main.appendChild(content);
+		
+		HtmlElement sidenav = new HtmlElement("aside");
+		sidenav.addClass("sidenav");
+		
+		HtmlElement sideNavList = new HtmlElement("ul");
+		sideNavList.addClass("sidenav-list");
+		sidenav.appendChild(sideNavList);
+		
+		for(WebinterfacePage pg : Webinterface.getPages()) {
+			HtmlElement sideNavListItem = new HtmlElement("li");
+			sideNavListItem.addClass("sidenav-list-item");
+			
+			HtmlElement a = new HtmlElement("a");
+			a.setText(pg.getName());
+			a.setAttribute("href", pg.getUrl());
+			sideNavListItem.appendChild(a);
+			
+			sideNavList.appendChild(sideNavListItem);
 		}
 		
+		d.getBodyNode().appendChild(header);
+		d.getBodyNode().appendChild(main);
+		d.getBodyNode().appendChild(sidenav);
 		d.getHeadNode().appendChild(HtmlElement.script(sc));
 		d.includeScript("/_internal/include.js", true);
+		d.includeScript("https://code.jquery.com/jquery-3.4.1.min.js", true);
 		d.addStyleSheet("/_internal/include.css");
 		return d;
 	}
