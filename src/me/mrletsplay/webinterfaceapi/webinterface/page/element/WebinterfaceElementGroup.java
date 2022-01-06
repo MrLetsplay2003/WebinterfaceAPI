1fcdddfabe4dc0b064a29691be65e5d1249c1bbd
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-01-06 16:58:28 +0100

    Allow dynamic content in groups

--- src/me/mrletsplay/webinterfaceapi/webinterface/page/element/WebinterfaceElementGroup.java
+++ src/me/mrletsplay/webinterfaceapi/webinterface/page/element/WebinterfaceElementGroup.java
@@ -2,17 +2,33 @@ package me.mrletsplay.webinterfaceapi.webinterface.page.element;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import me.mrletsplay.mrcore.json.JSONArray;
+import me.mrletsplay.mrcore.json.JSONObject;
 import me.mrletsplay.webinterfaceapi.html.HtmlElement;
+import me.mrletsplay.webinterfaceapi.webinterface.page.action.WebinterfaceRequestEvent;
+import me.mrletsplay.webinterfaceapi.webinterface.page.action.WebinterfaceResponse;
 import me.mrletsplay.webinterfaceapi.webinterface.page.element.layout.DefaultLayoutOption;
 
 public class WebinterfaceElementGroup extends AbstractWebinterfacePageElement {
 
 	private List<WebinterfacePageElement> elements;
 	
+	private WebinterfacePageElement templateElement;
+	
+	private String
+		dataRequestTarget,
+		dataRequestMethod;
+	
 	public WebinterfaceElementGroup() {
 		this.elements = new ArrayList<>();
+	}
+	
+	public WebinterfaceElementGroup(WebinterfacePageElement templateElement) {
+		this();
+		this.templateElement = templateElement;
 	}
 	
 	public void addTitle(Supplier<String> title) {
@@ -30,14 +46,61 @@ public class WebinterfaceElementGroup extends AbstractWebinterfacePageElement {
 		elements.add(element);
 	}
 	
+	public List<WebinterfacePageElement> getElements() {
+		return elements;
+	}
+	
+	public void setTemplateElement(WebinterfacePageElement templateElement) {
+		this.templateElement = templateElement;
+	}
+	
+	public WebinterfacePageElement getTemplateElement() {
+		return templateElement;
+	}
+	
+	public void setDataHandler(String requestTarget, String requestMethod) {
+		this.dataRequestTarget = requestTarget;
+		this.dataRequestMethod = requestMethod;
+	}
+	
+	public String getDataRequestTarget() {
+		return dataRequestTarget;
+	}
+	
+	public String getDataRequestMethod() {
+		return dataRequestMethod;
+	}
+	
 	@Override
 	public HtmlElement createElement() {
 		HtmlElement el = new HtmlElement("div");
 		el.addClass("grid-layout");
-		for(WebinterfacePageElement e : elements) {
-			el.appendChild(e.toHtml());
+		
+		if(templateElement != null) {
+			if(!templateElement.isTemplate()) throw new IllegalStateException("Template element is not a template");
+			el.addClass("dynamic-group");
+			el.setAttribute("data-dataRequestTarget", dataRequestTarget);
+			el.setAttribute("data-dataRequestMethod", dataRequestMethod);
+			el.setAttribute("data-template", templateElement.toHtml().toString());
+		}else {
+			for(WebinterfacePageElement e : elements) {
+				el.appendChild(e.toHtml());
+			}
 		}
+		
 		return el;
+	}
+	
+	public static <T> WebinterfaceResponse handleData(WebinterfaceRequestEvent event, List<T> items, Function<T, JSONObject> objectFunction) {
+		JSONArray elements = new JSONArray();
+		for(T o : items) {
+			JSONObject obj = objectFunction.apply(o);
+			elements.add(obj);
+		}
+		
+		JSONObject obj = new JSONObject();
+		obj.put("elements", elements);
+		return WebinterfaceResponse.success(obj);
 	}
 
 }
